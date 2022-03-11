1212849b347678208225bc3eab225282c7872233
damak <akinlade.co@gmail.com>
2022-03-11 19:05:00 +0100

    _document.js

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+// pages/_document.js
+
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Anonymous+Pro:ital,wght@0,400;0,700;1,400&family=Lato:wght@300;400;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
