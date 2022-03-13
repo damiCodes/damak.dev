0677b46ec786d0956f6a320ef41f14678bdc0d5d
damak <akinlade.co@gmail.com>
2022-03-13 14:17:50 +0100

    added icons to navigation links

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -1,4 +1,9 @@
 import Link from "next/link";
+import ProfileIcon from "../../public/svg/profile-icon";
+import TerminalIcon from "../../public/svg/terminal-icon";
+import WorkIcon from "../../public/svg/work-icon";
+import ProjectIcon from "../../public/svg/project-icon";
+import MailIcon from "../../public/svg/mail-icon";
 import classes from "./navigation.module.scss";
 
 export default function Navigation() {
@@ -9,19 +14,43 @@ export default function Navigation() {
       </div>
       <ul>
         <li>
-          <Link href="#about">{`<About />`}</Link>
+          <Link href="#about">
+            <a>
+              <ProfileIcon /> About
+            </a>
+          </Link>
         </li>
         <li>
-          <Link href="#arsenal">{`<Arsenal />`}</Link>
+          <Link href="#arsenal">
+            <a>
+              <TerminalIcon />
+              Arsenal
+            </a>
+          </Link>
         </li>
         <li>
-          <Link href="#experience">{`<Experience />`}</Link>
+          <Link href="#experience">
+            <a>
+              <WorkIcon />
+              Experience
+            </a>
+          </Link>
         </li>
         <li>
-          <Link href="#playground">{`<Playground />`}</Link>
+          <Link href="#playground">
+            <a>
+              <ProjectIcon />
+              Playground
+            </a>
+          </Link>
         </li>
         <li>
-          <Link href="#contact">{`<Contact />`}</Link>
+          <Link href="#contact">
+            <a>
+              <MailIcon />
+              Contact
+            </a>
+          </Link>
         </li>
       </ul>
       <div className={classes.footer}>
