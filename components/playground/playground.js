9d90483067d08829c986f48e36a3159c4fc41441
damak <akinlade.co@gmail.com>
2022-03-13 06:58:01 +0100

    playground section template

---
+++ components/playground/playground.js
@@ -0,0 +1,6 @@
+import Section from "../ui/section/";
+import classes from "./playground.module.scss";
+
+export default function Playground() {
+  return <Section id="playground">playground</Section>;
+}
