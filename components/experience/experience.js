abb1b0f42c6fe88e478bce249459f4253260094f
damak <akinlade.co@gmail.com>
2022-03-13 06:59:41 +0100

    experience section template

---
+++ components/experience/experience.js
@@ -0,0 +1,6 @@
+import Section from "../ui/section/";
+import classes from "./experience.module.scss";
+
+export default function Experience() {
+  return <Section id="experience">Experience</Section>;
+}
