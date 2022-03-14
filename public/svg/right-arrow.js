cb482602021a76262e5f29cdf90a6ec1c912a5fe
damak <akinlade.co@gmail.com>
2022-03-14 14:56:57 +0100

    svg icons

---
+++ public/svg/right-arrow.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function RightArrow() {
+  return (
+    <svg
+      width="22"
+      height="22"
+      viewBox="0 0 72 22"
+      xmlns="http://www.w3.org/2000/svg"
+      className="bow-arrow"
+    >
+      <path
+        fill="none"
+        stroke="#ffffff"
+        strokeWidth="4"
+        strokeMiterlimit="0"
+        d="M.043 11.119h70.714M60.917 1.319l9.8 9.8-9.8 9.8"
+      ></path>
+    </svg>
+  );
+}
