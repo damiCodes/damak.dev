9bc5d5cc53e122aec3096dfac8a23854e73e06dd
damak <akinlade.co@gmail.com>
2022-03-14 14:58:55 +0100

    added right arrow to button component

--- components/ui/button/button.js
+++ components/ui/button/button.js
@@ -1,11 +1,17 @@
 import Link from "next/link";
+import RightArrow from "../../../public/svg/right-arrow";
 import classes from "./button.module.scss";
 
 export default function Button({ href, children }) {
   return (
     <div className={classes.btn}>
       <Link href={`#${href}`}>
-        <a>{children}</a>
+        <a>
+          {children}{" "}
+          <span>
+            <RightArrow />
+          </span>
+        </a>
       </Link>
     </div>
   );
