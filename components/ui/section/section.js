fce11b41bad1f6cd646d50189cea2aa15018e1b7
damak <akinlade.co@gmail.com>
2022-03-16 20:43:31 +0100

    added background text to section component

--- components/ui/section/section.js
+++ components/ui/section/section.js
@@ -1,14 +1,16 @@
+import BackgroundText from "../background-text";
 import classes from "./section.module.scss";
 
-export default function Section({ children, id, heading, tagline }) {
+export default function Section({ children, id, heading, tagline, bgText }) {
   return (
     <section id={id} className={classes.section}>
       <div className={classes.heading}>
         <h2>{heading}</h2>
-        <p>{tagline}</p>
+        <h3>{tagline}</h3>
         <hr />
       </div>
       <div className={classes.content}>{children}</div>
+      <BackgroundText bgText={bgText} />
     </section>
   );
 }
