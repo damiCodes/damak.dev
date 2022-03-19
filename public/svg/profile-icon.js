33d1eeecebbb5dab6f7d573edd4acbd25fec9867
damak <akinlade.co@gmail.com>
2022-03-19 05:27:06 +0100

    changed nav icons from outline to solid fill

--- public/svg/profile-icon.js
+++ public/svg/profile-icon.js
@@ -4,16 +4,14 @@ export default function ProfileIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth={2}
+      className="h-5 w-5"
+      viewBox="0 0 20 20"
+      fill="currentColor"
     >
       <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+        fillRule="evenodd"
+        d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+        clipRule="evenodd"
       />
     </svg>
   );
