4fdb86d3128fae7d26c0d159d6cb781c55a4d1de
damak <akinlade.co@gmail.com>
2022-03-19 08:35:12 +0100

    added animation to header and css cleanup

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -1,25 +1,58 @@
 import Link from "next/link";
+import { motion } from "framer-motion";
+import Button from "../ui/button";
+import SocialIcons from "../ui/social-icons";
+import BackgroundText from "../ui/background-text";
 import classes from "./hero.module.scss";
 
 export default function Hero() {
   return (
-    <header>
-      <div className={classes.intro}>
+    <header className={classes.header}>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          y: [-300, 0],
+          opacity: 1,
+        }}
+        transition={{
+          type: "tween",
+          duration: 1,
+        }}
+      >
+        <h4>Hi, I&apos;m</h4>
+        <div className={classes.intro}>
+          <h1>Damak.</h1>
+        </div>
+      </motion.div>
+      <motion.div
+        initial={{ x: 60, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+        className={classes["tag-line"]}
+      >
         <h1>
-          Hi, <br />
-          I&apos;m Damak.
+          Creative Front-end
+          <br />
+          developer.
         </h1>
-      </div>
-      <div className={classes["tag-line"]}>
-        <h2>Creative Front-end developer.</h2>
-      </div>
-      <div className={classes.sub}>
-        <p>I write clean and efficient code.</p>
-      </div>
-      <div className={classes.btn}>
-        <Link href="#contact">Contact me!</Link>
-      </div>
-      {/* <div className={classes["background-text"]}>Hello There</div> */}
+      </motion.div>
+      <motion.div
+        initial={{ y: 30, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+        className={classes.sub}
+      >
+        <h3>I write clean and efficient code.</h3>
+      </motion.div>
+      <motion.div
+        initial={{ x: -250, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <Button href="#contact">Contact me!</Button>
+      </motion.div>
+      <SocialIcons />
+      <BackgroundText bgText="hello there!" />
     </header>
   );
 }
