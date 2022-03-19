cd9f3174c7c61589ffee23819845c87cf7fa7162
damak <akinlade.co@gmail.com>
2022-03-19 06:11:46 +0100

    removed social icons from navbar and styled logo

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -1,62 +1,36 @@
-import Link from "next/link";
 import NavLink from "./nav-link/nav-link";
+import HomeIcon from "../../public/svg/home-icon";
 import ProfileIcon from "../../public/svg/profile-icon";
 import TerminalIcon from "../../public/svg/terminal-icon";
 import WorkIcon from "../../public/svg/work-icon";
 import ProjectIcon from "../../public/svg/project-icon";
 import MailIcon from "../../public/svg/mail-icon";
-import GithubIcon from "../../public/svg/github-icon";
-import LinkedinIcon from "../../public/svg/linkedin-icon";
-import TwitterIcon from "../../public/svg/twitter-icon";
-import InstagramIcon from "../../public/svg/instagram-icon";
 import classes from "./navigation.module.scss";
 
 export default function Navigation() {
   return (
     <nav className={classes.nav}>
-      <div className={classes.logo}>
-        <Link href="/">{`<d />`}</Link>
-      </div>
+      <div className={classes.logo}>{/* <Link href="/">{`<d/>`}</Link> */}</div>
       <ul>
-        <NavLink href="about">
+        <NavLink href="/" active="active">
+          <HomeIcon /> Home
+        </NavLink>
+        <NavLink href="/#about">
           <ProfileIcon /> About
         </NavLink>
-        <NavLink href="arsenal">
+        <NavLink href="/#arsenal">
           <TerminalIcon /> Arsenal
         </NavLink>
-        <NavLink href="experience">
+        <NavLink href="/#experience">
           <WorkIcon /> Experience
         </NavLink>
-        <NavLink href="playground">
+        <NavLink href="/#playground">
           <ProjectIcon /> Playground
         </NavLink>
-        <NavLink href="contact">
+        <NavLink href="/#contact">
           <MailIcon /> Contact
         </NavLink>
       </ul>
-      <div className={classes.footer}>
-        <Link href="/">
-          <a>
-            <GithubIcon />
-          </a>
-        </Link>
-
-        <Link href="/">
-          <a>
-            <TwitterIcon />
-          </a>
-        </Link>
-        <Link href="/">
-          <a>
-            <InstagramIcon />
-          </a>
-        </Link>
-        <Link href="/">
-          <a>
-            <LinkedinIcon />
-          </a>
-        </Link>
-      </div>
     </nav>
   );
 }
