66367cd7c9406ab7ef91cc8e0da8e4f61fed80bd
damak <akinlade.co@gmail.com>
2022-03-19 06:04:02 +0100

    playground component

--- components/playground/playground.js
+++ components/playground/playground.js
@@ -1,6 +1,38 @@
+import { useState } from "react";
 import Section from "../ui/section/";
+import AllProjects from "./all-projects";
 import classes from "./playground.module.scss";
 
 export default function Playground() {
-  return <Section id="playground">playground</Section>;
+  const [activeTab, setActiveTab] = useState("all");
+
+  return (
+    <Section
+      id="playground"
+      heading="playground"
+      tagline="what i have done so far"
+      bgText="projects"
+    >
+      <nav className={classes.tabs}>
+        <button
+          style={{
+            backgroundColor: activeTab === "all" ? "var(--accent-color)" : null,
+          }}
+          onClick={() => setActiveTab("all")}
+        >
+          All
+        </button>
+        <button
+          style={{
+            backgroundColor:
+              activeTab === "featured" ? "var(--accent-color)" : null,
+          }}
+          onClick={() => setActiveTab("featured")}
+        >
+          Featured
+        </button>
+      </nav>
+      {activeTab === "all" && <AllProjects />}
+    </Section>
+  );
 }
