81b40cf1ee008c4d8de15f4a3ea096a3e854072f
damak <akinlade.co@gmail.com>
2022-03-19 06:09:31 +0100

    added active class prop, hover effect and css cleanup

--- components/navigation/nav-link/nav-link.js
+++ components/navigation/nav-link/nav-link.js
@@ -1,10 +1,11 @@
+import { useState } from "react";
 import Link from "next/link";
 import classes from "./nav-link.module.scss";
 
-export default function NavLink({ children, href }) {
+export default function NavLink({ children, href, active }) {
   return (
-    <li className={classes.li}>
-      <Link href={`#${href}`}>
+    <li className={`${classes.li} ${classes[active]}`}>
+      <Link href={href}>
         <a>{children}</a>
       </Link>
     </li>
