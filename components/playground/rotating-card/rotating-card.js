57a7de502958fa05c097f0bc0c3dd70004629f42
damak <akinlade.co@gmail.com>
2022-03-19 06:01:01 +0100

    rotating card component

---
+++ components/playground/rotating-card/rotating-card.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import classes from "./rotating-card.module.scss";
+
+export default function RotatingCard({ children }) {
+  return (
+    <div className={classes.body}>
+      <div className={classes.front}>
+        <div className={classes.image}></div>
+        <div className={classes.stack}>
+          <h3>Sample Project</h3>
+          <span>React</span>
+          <span>CSS</span>
+          <span>React</span>
+          <span>Firebase</span>
+          {/* <span>Firebase</span> */}
+        </div>
+      </div>
+      <div className={classes.back}>
+        <div className={classes.description}>
+          <h3>About</h3>
+          <hr />
+          <p>{children}</p>
+        </div>
+        <div className={classes.btn}>
+          <Link href="/">Go to Github</Link>
+          <Link href="/">Go to App</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
