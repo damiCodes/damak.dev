60f134021ce4751bf43e0353691acfc49f98bebb
damak <akinlade.co@gmail.com>
2022-06-05 00:42:04 +0100

    added mobile nav

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -1,6 +1,10 @@
+import { useState, useEffect } from "react";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
+import { motion } from "framer-motion";
 import NavLink from "./nav-link/nav-link";
 import HomeIcon from "../../public/svg/home-icon";
 import ProfileIcon from "../../public/svg/profile-icon";
+import Logo from "../../public/svg/logo";
 import TerminalIcon from "../../public/svg/terminal-icon";
 import WorkIcon from "../../public/svg/work-icon";
 import ProjectIcon from "../../public/svg/project-icon";
@@ -8,29 +12,83 @@ import MailIcon from "../../public/svg/mail-icon";
 import classes from "./navigation.module.scss";
 
 export default function Navigation() {
+  const { width } = useWindowDimensions();
+
+  const links = [
+    { href: "/", icon: <HomeIcon />, text: "home" },
+    { href: "/#about", icon: <ProfileIcon />, text: "about" },
+    { href: "/#arsenal", icon: <TerminalIcon />, text: "arsenal" },
+    { href: "/#experience", icon: <WorkIcon />, text: "experience" },
+    { href: "/#playground", icon: <ProjectIcon />, text: "playground" },
+    { href: "/#contact", icon: <MailIcon />, text: "contact" },
+  ];
+
   return (
-    <nav className={classes.nav}>
-      <div className={classes.logo}>{/* <Link href="/">{`<d/>`}</Link> */}</div>
-      <ul>
-        <NavLink href="/" active="active">
-          <HomeIcon /> Home
-        </NavLink>
-        <NavLink href="/#about">
-          <ProfileIcon /> About
-        </NavLink>
-        <NavLink href="/#arsenal">
-          <TerminalIcon /> Arsenal
-        </NavLink>
-        <NavLink href="/#experience">
-          <WorkIcon /> Experience
-        </NavLink>
-        <NavLink href="/#playground">
-          <ProjectIcon /> Playground
-        </NavLink>
-        <NavLink href="/#contact">
-          <MailIcon /> Contact
-        </NavLink>
-      </ul>
-    </nav>
+    <>
+      {width > 480 && (
+        <motion.nav
+          initial={{ x: -60, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.4 }}
+          className={classes.nav}
+        >
+          <motion.div
+            initial={{ scale: 0, translateX: "-50%" }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.7, delay: 0.5 }}
+            className={classes.logo}
+          >
+            <Logo width="30px" fill="#ffffff" />
+          </motion.div>
+          <ul>
+            {links.map((link, index) => {
+              return (
+                <motion.div
+                  key={link.text}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: index * 0.2 }}
+                >
+                  <NavLink href={link.href}>
+                    {link.icon}
+                    {link.text}
+                  </NavLink>
+                </motion.div>
+              );
+            })}
+          </ul>
+        </motion.nav>
+      )}
+
+      {width <= 480 && (
+        <nav className={classes["mobile-nav"]}>
+          <motion.div
+            initial={{ scale: 0, translateX: "-50%" }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.7, delay: 0.5 }}
+            className={classes.logo}
+          >
+            <Logo width="20px" fill="#ffffff" />
+          </motion.div>
+          <ul>
+            {links.map((link, index) => {
+              return (
+                <motion.div
+                  key={link.text}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: index * 0.2 }}
+                >
+                  <NavLink href={link.href}>
+                    {link.icon}
+                    {link.text}
+                  </NavLink>
+                </motion.div>
+              );
+            })}
+          </ul>
+        </nav>
+      )}
+    </>
   );
 }
