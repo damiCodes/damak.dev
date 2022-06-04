eb9fd11a0cb5331a68f93a574aa26e860b75646d
damak <akinlade.co@gmail.com>
2022-06-05 00:24:33 +0100

    logo

---
+++ public/svg/logo.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+export default function Logo(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 84.23 83.11"
+      {...props}
+    >
+      <defs>
+        <style>.cls-1</style>
+      </defs>
+      <g id="Layer_2" data-name="Layer 2">
+        <g id="Layer_1-2" data-name="Layer 1">
+          <path
+            className="cls-1"
+            d="M42.68,0V14.75a26.81,26.81,0,0,1,0,53.61V83.11A41.56,41.56,0,0,0,42.68,0Z"
+          />
+          <rect className="cls-1" x="11.23" width="31.81" height="14.72" />
+          <rect
+            className="cls-1"
+            x="11.23"
+            y="68.39"
+            width="31.78"
+            height="14.72"
+          />
+          <rect
+            className="cls-1"
+            x="0.05"
+            y="22.55"
+            width="41.46"
+            height="11.23"
+          />
+          <rect
+            className="cls-1"
+            x="0.05"
+            y="49.33"
+            width="41.47"
+            height="11.23"
+          />
+          <rect className="cls-1" y="22.53" width="11.23" height="38.03" />
+          <path
+            className="cls-1"
+            d="M41.29,22.55V33.78a8.07,8.07,0,0,1,8.33,7.78,8.07,8.07,0,0,1-8.33,7.77V60.56c11.25,0,20.37-8.51,20.37-19S52.54,22.55,41.29,22.55Z"
+          />
+        </g>
+      </g>
+    </svg>
+  );
+}
