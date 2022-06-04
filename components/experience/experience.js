ad13a0296c3833a6099eccf0b83b9433be4134db
damak <akinlade.co@gmail.com>
2022-06-05 00:44:30 +0100

    work experiences

--- components/experience/experience.js
+++ components/experience/experience.js
@@ -1,66 +1,11 @@
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import Section from "../ui/section/";
+import { Swg, Hng, Ris, Gni } from "./items/items";
 import classes from "./experience.module.scss";
 
 export default function Experience() {
   const [activeTab, setActiveTab] = useState("swg");
-
-  const swg = (
-    <>
-      <h3>Front-end Developer</h3>
-      <p>August - December 2021</p>
-      <ul>
-        <li>
-          Built beautiful user interfaces for the Swirge social platform using
-          ReactJS.
-        </li>
-        <li>
-          Worked in an agile environment to receive design requirements, and
-          test ih applications.
-        </li>
-      </ul>
-    </>
-  );
-
-  const hng = (
-    <>
-      <h3>Intern</h3>
-      <p>August - October 2021</p>
-      <ul>
-        <li>
-          Built responsive user Interfaces for the Zuri Chat App using ReactJS
-        </li>
-        <li>
-          Collaborated with my team to build a File Manager plugin for Zuri Chat
-        </li>
-        <li>Completed the internship and emerged a finalist</li>
-      </ul>
-    </>
-  );
-
-  const ris = (
-    <>
-      <h3>Intern</h3>
-      <p>August - November 2019</p>
-      <ul>
-        <li>Built company webpages with wordpress</li>
-        <li>UI Design</li>
-        <li>Graphics Design for social media posts</li>
-      </ul>
-    </>
-  );
-
-  const gni = (
-    <>
-      <h3>Intern</h3>
-      <p>October 2018 - March 2019</p>
-      <ul>
-        <li>Hardware maintenance and repair</li>
-        <li> Network troubleshooting and configuration</li>
-        <li>Database Management</li>
-      </ul>
-    </>
-  );
 
   return (
     <Section
@@ -70,7 +15,13 @@ export default function Experience() {
       bgText="experiences"
     >
       <div className={classes.tab}>
-        <ul className={classes["tab-list"]}>
+        <motion.ul
+          initial={{ x: -200, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ delay: 0.7, duration: 0.6 }}
+          className={classes["tab-list"]}
+        >
           <li
             className={activeTab === "swg" ? classes.active : null}
             onClick={() => setActiveTab("swg")}
@@ -95,13 +46,21 @@ export default function Experience() {
           >
             Great Nigeria Insurance
           </li>
-        </ul>
-        <div className={classes["tab-content"]}>
-          {activeTab === "swg" && swg}
-          {activeTab === "hng" && hng}
-          {activeTab === "ris" && ris}
-          {activeTab === "gni" && gni}
-        </div>
+        </motion.ul>
+        <AnimatePresence exitBeforeEnter>
+          <motion.div
+            initial={{ x: 200, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ delay: 0.9, duration: 0.6 }}
+            className={classes["tab-content"]}
+          >
+            {activeTab === "swg" && <Swg />}
+            {activeTab === "hng" && <Hng />}
+            {activeTab === "ris" && <Ris />}
+            {activeTab === "gni" && <Gni />}
+          </motion.div>
+        </AnimatePresence>
       </div>
     </Section>
   );
---
+++ components/experience/items/items.js
@@ -0,0 +1,86 @@
+import { motion } from "framer-motion";
+
+export function Swg() {
+  return (
+    <motion.div
+      animate={{ opacity: 1, y: 0 }}
+      initial={{ opacity: 0, y: 30 }}
+      // exit={{ opacity: 0, y: -30 }}
+      transition={{ duration: 0.5 }}
+    >
+      <h3>Front-end Developer</h3>
+      <p>August - December 2021</p>
+      <ul>
+        <li>
+          Built beautiful user interfaces for the Swirge social platform using
+          ReactJS.
+        </li>
+        <li>
+          Worked in an agile environment to receive design requirements, and
+          test ih applications.
+        </li>
+      </ul>
+    </motion.div>
+  );
+}
+
+export function Hng() {
+  return (
+    <motion.div
+      animate={{ opacity: 1, y: 0 }}
+      initial={{ opacity: 0, y: 30 }}
+      // exit={{ opacity: 0, y: -30 }}
+      transition={{ duration: 0.5 }}
+    >
+      <h3>Intern</h3>
+      <p>August - October 2021</p>
+      <ul>
+        <li>
+          Built responsive user Interfaces for the Zuri Chat App using ReactJS
+        </li>
+        <li>
+          Collaborated with my team to build a File Manager plugin for Zuri Chat
+        </li>
+        <li>Completed the internship and emerged a finalist</li>
+      </ul>
+    </motion.div>
+  );
+}
+
+export function Ris() {
+  return (
+    <motion.div
+      animate={{ opacity: 1, y: 0 }}
+      initial={{ opacity: 0, y: 30 }}
+      // exit={{ opacity: 0, y: -30 }}
+      transition={{ duration: 0.5 }}
+    >
+      <h3>Intern</h3>
+      <p>August - November 2019</p>
+      <ul>
+        <li>Built company webpages with wordpress</li>
+        <li>UI Design</li>
+        <li>Graphics Design for social media posts</li>
+      </ul>
+    </motion.div>
+  );
+}
+
+export function Gni() {
+  return (
+    <motion.div
+      animate={{ opacity: 1, y: 0 }}
+      initial={{ opacity: 0, y: 30 }}
+      // exit={{ opacity: 0, y: -30 }}
+      transition={{ duration: 0.5 }}
+    >
+      <h3>Intern</h3>
+      <p>October 2018 - March 2019</p>
+      <ul>
+        <li>Hardware maintenance and repair</li>
+        <li> Network troubleshooting and configuration</li>
+        <li>Database Management</li>
+      </ul>
+    </motion.div>
+  );
+}
