7eb5bc8ef5e740d174e93b1cd4d3f22132000fe0
damak <akinlade.co@gmail.com>
2022-06-05 00:43:50 +0100

    get

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,13 +1,15 @@
 import classes from "./footer.module.scss";
 
 export default function Footer() {
+  const year = new Date().getFullYear();
+
   return (
     <footer className={classes.footer}>
       <div className={classes.content}>
         <p>
           Designed & built by <span>Damilola Akinlade</span>
         </p>
-        <p>Copyright &copy; 2022</p>
+        <p>Copyright &copy; {year}</p>
       </div>
     </footer>
     // <div></div>
