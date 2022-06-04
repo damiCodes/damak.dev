272396d1d08a1c1a70454c6e7dfbce804ef54702
damak <akinlade.co@gmail.com>
2022-06-05 00:45:00 +0100

    contact section

--- components/contact/contact.js
+++ components/contact/contact.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import Link from "next/link";
 import BeatLoader from "react-spinners/BeatLoader";
 import Section from "../ui/section/";
@@ -73,7 +74,13 @@ export default function Contact() {
   return (
     <Section id="contact" heading="get in touch." tagline="let's Talk!">
       <div className={classes.body}>
-        <div className={classes.text}>
+        <motion.div
+          initial={{ x: -200, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ delay: 0.7, duration: 0.6 }}
+          className={classes.text}
+        >
           <p>
             My inbox is always open. You can shoot me a mail at{" "}
             <span>
@@ -81,9 +88,16 @@ export default function Contact() {
             </span>
             or send a message via the form.
           </p>
-          <SocialIcons />
-        </div>
-        <form className={classes.form} onSubmit={submitHandler}>
+          <SocialIcons delay={0.8} />
+        </motion.div>
+        <motion.form
+          initial={{ x: 200, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ delay: 0.9, duration: 0.6 }}
+          className={classes.form}
+          onSubmit={submitHandler}
+        >
           <div className={classes["form-group"]}>
             <input
               type="text"
@@ -126,7 +140,7 @@ export default function Contact() {
               )}
             </button>
           </div>
-        </form>
+        </motion.form>
         <div className={`${classes.alert} ${classes[alertType]}`}>{alert}</div>
       </div>
     </Section>
