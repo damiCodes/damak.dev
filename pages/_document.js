8ed065944366acf565c677a997b7701657ebc7d8
damak <akinlade.co@gmail.com>
2022-06-05 00:33:19 +0100

    added default language to custom document

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Alike+Angular&family=Anonymous+Pro:ital,wght@0,400;0,700;1,400&family=Lato:wght@300;400;700;900&display=swap"
