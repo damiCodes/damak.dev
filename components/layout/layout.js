d853162ca5773e9d4b7d2ab6ed8f4337a2e33500
damak <akinlade.co@gmail.com>
2022-06-05 00:42:32 +0100

    added progress icon

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,6 +1,7 @@
 import Navigation from "../navigation";
 import Wrapper from "../ui/wrapper";
 import Footer from "../footer";
+import ProgressIcon from "../ui/progress-icon/progress-icon";
 import classes from "./layout.module.scss";
 
 export default function Layout({ children }) {
@@ -11,6 +12,7 @@ export default function Layout({ children }) {
         {children}
         <Footer />
       </Wrapper>
+      <ProgressIcon />
     </div>
   );
 }
