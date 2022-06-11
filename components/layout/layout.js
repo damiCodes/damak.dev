0db27b0951b0b5e277b6dfcca4b621b6715c76b4
damak <akinlade.co@gmail.com>
2022-06-11 02:51:33 +0100

    disabled progress icon

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -12,7 +12,7 @@ export default function Layout({ children }) {
         {children}
         <Footer />
       </Wrapper>
-      <ProgressIcon />
+      {/* <ProgressIcon /> */}
     </div>
   );
 }
