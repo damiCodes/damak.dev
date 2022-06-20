b435d318552ee020a8687ac212a20600acba0738
damak <akinlade.co@gmail.com>
2022-06-20 15:48:24 +0100

    updated resume link

--- components/about/about.js
+++ components/about/about.js
@@ -20,25 +20,44 @@ export default function About() {
           initial={{ x: -200, opacity: 0 }}
           whileInView={{ x: 0, opacity: 1 }}
           viewport={{ once: true }}
-          transition={{ delay: 0.9, duration: 0.6 }}
+          transition={{ delay: 0.6, duration: 0.6 }}
           className={classes.text}
         >
-          My name is Damilola Akinlade. I am a Computer Engineer, front-end
-          developer, and Javascript enthusiast, based in Lagos, Nigeria. I am
-          very passionate about building things for the web. My superpower is
-          building delightful user interfaces with good ol’ vanilla CSS.
+          My name is Damilola Akinlade. I am a Computer Engineer, ambitious
+          front-end developer, and Javascript enthusiast, based in Lagos,
+          Nigeria. I am very passionate about building things for the web. My
+          superpower is building delightful user interfaces with vanilla CSS.
           <br />
           You can find me at my workspace cackling on my keyboard trying to
           tackle technical problems in the best way possible. Being diligent and
           result-oriented, I work towards achieving the best results on every
           project I partake in.
         </motion.p>
-        {width <= 800 && <Button href="/resume">Resume</Button>}
+        {width <= 800 && (
+          <Button
+            href="https://drive.google.com/file/d/1gkGii5S89Xp4vda9RFK_jw6yioDWED5m/view?usp=sharing"
+            target="_blank"
+          >
+            Resume
+          </Button>
+        )}
         <div className={classes.image}>
-          <Programmer />
+          <Programmer
+            initial={{ x: 200, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ delay: 0.6, duration: 0.6 }}
+          />
         </div>
       </div>
-      {width > 800 && <Button href="/resume">Resume</Button>}
+      {width > 800 && (
+        <Button
+          href="https://drive.google.com/file/d/1gkGii5S89Xp4vda9RFK_jw6yioDWED5m/view?usp=sharing"
+          target="_blank"
+        >
+          Resume
+        </Button>
+      )}
     </Section>
   );
 }
