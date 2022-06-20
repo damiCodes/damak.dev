aead51e89e03166bcba20a960a6d040ca891d832
damak <akinlade.co@gmail.com>
2022-06-20 16:05:57 +0100

    chnaged animation style

--- components/ui/pre-loader/pre-loader.js
+++ components/ui/pre-loader/pre-loader.js
@@ -9,7 +9,7 @@ export default function PreLoader() {
       transition: {
         duration: 1,
         delay: 2,
-        // ease: "easeInOut",
+        ease: "easeInOut",
       },
     },
     visible: {
@@ -25,7 +25,6 @@ export default function PreLoader() {
       scale: 1,
       transition: {
         duration: 1,
-        delay: 0.8,
         ease: "easeInOut",
       },
     },
@@ -41,6 +40,7 @@ export default function PreLoader() {
       pathLength: 1,
       transition: {
         duration: 1,
+        delay: 0.8,
         ease: "easeInOut",
       },
     },
@@ -64,18 +64,8 @@ export default function PreLoader() {
           style={{ transformOrigin: "50% 50% 0" }}
         >
           <motion.g variants={pathVariant} initial="hidden" animate="visible">
-            <path
-              variants={pathVariant}
-              initial="hidden"
-              animate="visible"
-              d="M112.37,73.05A38.65,38.65,0,0,0,73.91,34.53H44.21V48.17H73.57v0a24.85,24.85,0,1,1,0,49.69v0H44.21v13.65H73.88A38.65,38.65,0,0,0,112.37,73.05Z"
-            />
-            <path
-              variants={pathVariant}
-              initial="hidden"
-              animate="visible"
-              d="M91.29,73.05c0-9.67-8.4-17.51-18.81-17.62H33.72V90.67H72.49C82.89,90.55,91.29,82.71,91.29,73.05Zm-19,7.2H44.21V65.84H72.27a7.51,7.51,0,0,1,7.78,7.21C80.05,77,76.57,80.25,72.27,80.25Z"
-            />
+            <path d="M112.37,73.05A38.65,38.65,0,0,0,73.91,34.53H44.21V48.17H73.57v0a24.85,24.85,0,1,1,0,49.69v0H44.21v13.65H73.88A38.65,38.65,0,0,0,112.37,73.05Z" />
+            <path d="M91.29,73.05c0-9.67-8.4-17.51-18.81-17.62H33.72V90.67H72.49C82.89,90.55,91.29,82.71,91.29,73.05Zm-19,7.2H44.21V65.84H72.27a7.51,7.51,0,0,1,7.78,7.21C80.05,77,76.57,80.25,72.27,80.25Z" />
           </motion.g>
           <motion.circle
             variants={circleVariant}
