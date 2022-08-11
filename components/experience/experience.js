350d4bea9c498504b118ecbdee5454d78a1898a4
damak <akinlade.co@gmail.com>
2022-08-11 19:46:46 +0100

    updated experiences

--- components/experience/experience.js
+++ components/experience/experience.js
@@ -1,19 +1,19 @@
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Section from "../ui/section/";
-import { MyArtStock, Zozo, Swg, Hng, Ris } from "./items/items";
+import { OneFlare, BluColar, Swirge, Hng, Ris } from "./items/items";
 import classes from "./experience.module.scss";
 
-const data = [
-  { title: "My Art Stock", id: "art" },
-  { title: "Zozo", id: "zozo" },
+const list = [
+  { title: "One Flare", id: "one" },
+  { title: "BluColar", id: "blu" },
   { title: "Swirge", id: "swg" },
   { title: "HNG i8", id: "hng" },
   { title: "Reliance Infosystems", id: "ris" },
 ];
 
 export default function Experience() {
-  const [activeTab, setActiveTab] = useState(data[0].id);
+  const [activeTab, setActiveTab] = useState(list[0].id);
 
   return (
     <Section
@@ -30,7 +30,7 @@ export default function Experience() {
           transition={{ delay: 0.7, duration: 0.6 }}
           className={classes["tab-list"]}
         >
-          {data.map((item, index) => (
+          {list.map((item, index) => (
             <motion.li
               key={item.id}
               initial={{ opacity: 0, y: 30 }}
@@ -52,9 +52,9 @@ export default function Experience() {
             transition={{ delay: 0.9, duration: 0.6 }}
             className={classes["tab-content"]}
           >
-            {activeTab === "art" && <MyArtStock />}
-            {activeTab === "zozo" && <Zozo />}
-            {activeTab === "swg" && <Swg />}
+            {activeTab === "one" && <OneFlare />}
+            {activeTab === "blu" && <BluColar />}
+            {activeTab === "swg" && <Swirge />}
             {activeTab === "hng" && <Hng />}
             {activeTab === "ris" && <Ris />}
           </motion.div>
--- components/experience/items/items.js
+++ components/experience/items/items.js
@@ -5,51 +5,58 @@ const variants = {
   visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
 };
 
-export function MyArtStock() {
-  return (
-    <motion.div variants={variants} initial="hidden" animate="visible">
-      <h3>Front-end Developer</h3>
-      <p>Lagos, 2022</p>
-      <ul>
-        <li>Worked with team members to create responsive user interfaces.</li>
-        <li>Integrated software components with third party API.</li>
-        <li>Produced clean and efficient codes based on specifications.</li>
-      </ul>
-    </motion.div>
-  );
-}
-
-export function Zozo() {
+export function OneFlare() {
   return (
     <motion.div variants={variants} initial="hidden" animate="visible">
       <h3>Front-end Developer</h3>
       <p>Lagos, 2022</p>
       <ul>
         <li>
-          Built beautiful user interfaces for the Zozo bidding platform using
-          NextJS.
+          Revised and updated old code bases to modern development standards,
+          improving functionality.
         </li>
+        <li>Wrote clean and efficient codes based on specifications.</li>
+        <li>Integrated software components with third-party APIs.</li>
         <li>Troubleshoot, debug and upgrade existing software.</li>
-        <li>Integrated software components with third party API.</li>
       </ul>
     </motion.div>
   );
 }
 
-export function Swg() {
+export function BluColar() {
+  return (
+    <motion.div variants={variants} initial="hidden" animate="visible">
+      <h3>Front-end Developer</h3>
+      <p>Lagos, 2022</p>
+      <ul>
+        <li>
+          Wrote modern and performant code for clients and in-house projects.
+        </li>
+        <li>Integrated software components with third-party API.</li>
+        <li>Worked with team members to create responsive user interfaces.</li>
+        <li>
+          Collaborated on stages of software development lifecycle from
+          requirement gathering to production releases.
+        </li>
+      </ul>
+    </motion.div>
+  );
+}
+
+export function Swirge() {
   return (
     <motion.div variants={variants} initial="hidden" animate="visible">
       <h3>Front-end Developer</h3>
       <p>Abuja, 2021</p>
       <ul>
         <li>
-          Built beautiful user interfaces for the Swirge social platform using
-          ReactJS.
+          Built responsive user interfaces for the Swirge social platform.
         </li>
         <li>
-          Worked in an agile environment to receive design requirements, and
-          test ih applications.
+          Participated in software field testing to verify the performance of
+          developed projects.
         </li>
+        <li>Integrated software components with third-party API.</li>
       </ul>
     </motion.div>
   );
@@ -62,12 +69,18 @@ export function Hng() {
       <p>Lagos, 2021</p>
       <ul>
         <li>
-          Built responsive user Interfaces for the Zuri Chat App using ReactJS
+          Built responsive user Interfaces for the Zuri Chat App, a shared
+          workspace platform.
         </li>
         <li>
-          Collaborated with my team to build a File Manager plugin for Zuri Chat
+          Collaborated with a team of developers to build a File Manager plugin
+          for the workspace platform.
         </li>
-        <li>Completed the internship and emerged a finalist</li>
+        <li>
+          Collaborated with other developers to identify and alleviate bugs and
+          errors in software.
+        </li>
+        <li>Integrated software components with third-party API.</li>
       </ul>
     </motion.div>
   );
