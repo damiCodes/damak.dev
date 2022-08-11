00bea80f356d0f642da9637b927b61df60b1377a
damak <akinlade.co@gmail.com>
2022-08-11 17:16:21 +0100

    api route for contact form - fixed

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,57 +1,45 @@
 require("dotenv").config();
-import { createTransport } from "nodemailer";
-import { google } from "googleapis";
+const { createTransport } = require("nodemailer");
+const { google } = require("googleapis");
+
+const OAuth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+
+OAuth2Client.setCredentials({
+  refresh_token: process.env.REFRESH_TOKEN,
+});
 
 export default async function handler(req, res) {
   const { name, email, message } = req.body;
-  const data = { name, email, message };
-  const OAuth2 = google.auth.OAuth2;
-
-  const OAuth2Client = new OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    "https://developers.google.com/oauthplayground"
-  );
-
-  OAuth2Client.setCredentials({
-    refresh_token: process.env.REFRESH_TOKEN,
-  });
-
-  const accessToken = await new Promise((resolve, reject) => {
-    OAuth2Client.getAccessToken((err, token) => {
-      if (err) {
-        reject("Failed to create access token :(");
-      }
-      resolve(token);
-    });
-  });
-
-  const transporter = createTransport({
-    service: "gmail",
-    auth: {
-      type: "OAuth2",
-      user: process.env.EMAIL,
-      accessToken,
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      refreshToken: process.env.REFRESH_TOKEN,
-    },
-  });
 
   try {
-    const result = await transporter.sendMail({
-      from: process.env.EMAIL,
-      to: "damak.dev@gmail.com",
-      subject: `Message From ${data.name}`,
-      text: `${data.message} \n\nEmail Address: ${data.email}`,
+    const accessToken = await OAuth2Client.getAccessToken();
+    const transporter = createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.EMAIL,
+        // clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken,
+      },
     });
-    console.log(result);
-    res.status(200).json({ message: "Success ✔", type: "success" });
+
+    await transporter.sendMail({
+      from: process.env.EMAIL,
+      to: process.env.EMAIL,
+      subject: `Message From ${name}`,
+      text: `${message} \n\nEmail Address: ${email}`,
+    });
+    res.status(200).json({ message: "Message sent ✔", status: "success" });
   } catch (error) {
     res.status(400).json({
-      message: "An error occurred, please try again. ✖",
-      type: "error",
+      message: "An error occurred, please try again.",
+      status: "failed",
     });
-    console.log(error);
   }
 }
