f2065e16b8f12c54834f4158d919e63aa2964e07
damak <akinlade.co@gmail.com>
2022-08-11 08:00:45 +0100

    removed playground session

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <About />
         <Arsenal />
         <Experience />
-        <Playground />
+        {/* <Playground /> */}
         <Contact />
       </main>
     </>
