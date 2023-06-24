350993deffd39251667bc24cac985a37c9952f6a
Damilola Akinlade <damak.dev@gmail.com>
2023-06-24 01:20:33 +0100

    updated resume link

--- components/about/about.js
+++ components/about/about.js
@@ -8,7 +8,7 @@ import classes from "./about.module.scss";
 export default function About() {
   const { width } = useWindowDimensions();
   const resumeLink =
-    "https://drive.google.com/file/d/12ZYW3FefLx758l884SDkmcTcwhuk-_0k/view?usp=sharing";
+    "https://drive.google.com/file/d/1DI6v38W-hGSWycyfN5eDeyaPG4J3rTkd/view?usp=sharing";
 
   return (
     <Section
