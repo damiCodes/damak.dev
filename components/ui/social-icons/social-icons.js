283d597872e023d9e6332e397039ff52b3978035
Damilola Akinlade <damak.dev@gmail.com>
2023-11-09 07:46:45 +0100

    removed twitter

--- components/ui/social-icons/social-icons.js
+++ components/ui/social-icons/social-icons.js
@@ -15,27 +15,27 @@ export default function SocialIcons({ delay }) {
       }}
       className={classes["social-icons"]}
     >
-      <Link href="https://github.com/damakdev">
+      <Link href="https://github.com/0xdamak">
         <a target="_blank">
           <icons.Github />
         </a>
       </Link>
+      <Link href="https://www.linkedin.com/in/0xdamak">
+        <a target="_blank">
+          <icons.Linkedin />
+        </a>
+      </Link>
 
-      <Link href="https://twitter.com/__damak">
+      {/* <Link href="https://twitter.com/0xdamak">
         <a target="_blank">
           <icons.Twitter />
         </a>
-      </Link>
+      </Link> */}
       {/* <Link href="https://www.instagram.com/damak.eth/">
         <a target="_blank">
           <icons.Instagram />
         </a>
       </Link> */}
-      <Link href="https://www.linkedin.com/in/damilola-akinlade-059037189/">
-        <a target="_blank">
-          <icons.Linkedin />
-        </a>
-      </Link>
     </motion.div>
   );
 }
